8e29f2dba9085757053cd4af8006570ccb618748
milo <mybule8@naver.com>
2022-05-27 12:22:34 +0900

    Junit Repository Test 완료

---
+++ src/main/java/site/metacoding/blogv4junit/domain/book/Book.java
@@ -0,0 +1,35 @@
+package site.metacoding.blogv4junit.domain.book;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Builder;
+import lombok.Getter;
+
+
+@Getter
+@Entity
+public class Book {
+
+
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+     private Long id; // mapping 타입을 써라. 0이 들어오면 막는다.
+    private String title;
+    private String author;
+
+
+    @Builder
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    //필요한 생성자만 만든다. 
+    }
+
+
+
+}
+
